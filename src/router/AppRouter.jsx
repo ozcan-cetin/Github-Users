0ff37a1019c3525cc13ea608bbaace8c73df2da0
özcan çetin <cetin445@gmail.com>
2022-07-07 23:51:01 +0300

    v1

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from "../pages/About"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
-import Navbar from "../components/NavBar"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "../pages/About";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Navbar from "../components/NavBar";
+import NotFound from "../pages/NotFound";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
